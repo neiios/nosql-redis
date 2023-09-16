e298455c520e872087e12f194c072ccc10bd88fc
Igor <egor@sgf.lt>
2023-09-16 14:08:39 +0300

    Add a ui to manager reservations

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 - [x] Viewing a list of available and booked rooms.
 - [x] Making reservations for guests with specific room numbers and check-in/check-out dates.
 - [x] Canceling reservations.
-- [ ] Viewing a guest's reservation history.
+- [x] Manage reservation with a simple ui
 - [x] Use multi/exec to avoid overwriting booking information
--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class Hotel:
             )
             pipeline.execute()
         else:
-            print(f"Room {room_id} is already reserved.")
+            print(f"Can't reserve room {room_id}.")
 
     def remove_reservation(self, room_id: int) -> None:
         key = f"{self.hotel_name}:room:{room_id}"
@@ -59,7 +59,7 @@ class Hotel:
             pipeline.hset(key, "booked", 0)
             pipeline.execute()
         else:
-            print(f"Room {room_id} wasn't booked to begin with.")
+            print(f"Can't remove reservation for room {room_id}.")
 
     def get_room_list(self) -> ([(str, str)], [(str, str)]):
         key = f"{self.hotel_name}:room:*"
@@ -107,6 +107,45 @@ def main() -> None:
     hotel.remove_reservation(102)
     hotel.reserve_room(103, "Joe Biden", "2019-08-09", "2019-08-10")
     hotel.print_room_list(hotel.get_room_list())
+
+    while True:
+        print("\nHotel Management CLI")
+        print("---------------------")
+        print("1. Add Room")
+        print("2. Remove Room")
+        print("3. Reserve Room")
+        print("4. Remove Reservation")
+        print("5. Print Room List")
+        print("6. Exit")
+        print("---------------------")
+
+        choice = input("Enter your choice (1-6): ")
+
+        match choice:
+            case "1":
+                room_id = int(input("Enter room id: "))
+                hotel.add_room(room_id)
+                print(f"Room {room_id} added!")
+            case "2":
+                room_id = int(input("Enter room id: "))
+                hotel.remove_room(room_id)
+                print(f"Room {room_id} removed!")
+            case "3":
+                room_id = int(input("Enter room id: "))
+                name = input("Enter guest name: ")
+                start_date = input("Enter start date (YYYY-MM-DD): ")
+                end_date = input("Enter end date (YYYY-MM-DD): ")
+                hotel.reserve_room(room_id, name, start_date, end_date)
+            case "4":
+                room_id = int(input("Enter room id: "))
+                hotel.remove_reservation(room_id)
+            case "5":
+                hotel.print_room_list(hotel.get_room_list())
+            case "6":
+                print("Goodbye!")
+                break
+            case _:
+                print("Invalid choice! Please choose a valid option.")
 
 
 if __name__ == "__main__":
